d883d3c85413842b2dd6605f7eadf3c3de1c0779
Sandeep Manne <smanne@users.noreply.github.com>
2021-02-18 18:38:37 +0530

    Update longest-substring-without-repeating-characters.go

--- Algorithms/0003.longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ Algorithms/0003.longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,27 +1,35 @@
 package problem0003
 
+/**
+1. Initalize a array to store chars as there are max 128 chars create a fixed array of 128 ints to store the location of the char in given string
+2. Initilize maxLenght, start
+3. Iterate through given string and extract char
+4. If char is already present in charArray and the placement of the char is greater than start index then
+5. We found a duplicate char so we exclude everything before the previous positon of this char and move the start index to prev position + 1
+6. Also check if the maxLength is less than the current max 
+7. Insert char position in array to current position
+**/
 func lengthOfLongestSubstring(s string) int {
-	// location[s[i]] == j 表示：
-	// s中第i个字符串，上次出现在s的j位置，所以，在s[j+1:i]中没有s[i]
-	// location[s[i]] == -1 表示： s[i] 在s中第一次出现
-	location := [256]int{} // 只有256长是因为，假定输入的字符串只有ASCII字符
-	for i := range location {
-		location[i] = -1 // 先设置所有的字符都没有见过
-	}
-
-	maxLen, left := 0, 0
-
-	for i := 0; i < len(s); i++ {
-		// 说明s[i]已经在s[left:i+1]中重复了
-		// 并且s[i]上次出现的位置在location[s[i]]
-		if location[s[i]] >= left {
-			left = location[s[i]] + 1 // 在s[left:i+1]中去除s[i]字符及其之前的部分
-		} else if i+1-left > maxLen {
-			// fmt.Println(s[left:i+1])
-			maxLen = i + 1 - left
-		}
-		location[s[i]] = i
-	}
-
-	return maxLen
+    charArray := [128]int{}
+    start := 0
+    maxLength := 0
+    var i int
+    for i := range charArray {
+	    charArray[i] = -1 // as defualt value is 0, init with -1
+    }
+    for i = 0; i<len(s); i++ {
+        if charArray[s[i]] >= start {
+            //found duplicate
+            if (i - start) > maxLength {
+                maxLength = i - start 
+            }
+            start = charArray[s[i]]+1
+        }
+        
+        charArray[s[i]] = i
+    }
+    if (i - start) > maxLength {
+        maxLength = i - start 
+    }
+    return maxLength
 }
