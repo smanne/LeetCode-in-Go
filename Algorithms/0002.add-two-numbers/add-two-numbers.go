d3c6975c470522362087ad1562de42161f65ae20
Sandeep Manne <smanne@users.noreply.github.com>
2021-02-18 15:36:47 +0530

    Update add-two-numbers.go

--- Algorithms/0002.add-two-numbers/add-two-numbers.go
+++ Algorithms/0002.add-two-numbers/add-two-numbers.go
@@ -15,22 +15,28 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	resPre := &ListNode{}
 	cur := resPre
 	carry := 0
-
+        
+	// Iterate through lists until all of the list is nil
 	for l1 != nil || l2 != nil || carry > 0 {
+		// Initalize sum with previous carry
 		sum := carry
 
+		// Add l1 val to sum and move l1
 		if l1 != nil {
 			sum += l1.Val
 			l1 = l1.Next
 		}
 
+		// Add l2 val to sum and move l2 to next
 		if l2 != nil {
 			sum += l2.Val
 			l2 = l2.Next
 		}
 
+		// get carry if sum is greater than 10
 		carry = sum / 10
 
+		// store mod 10 val in curr node next
 		cur.Next = &ListNode{Val: sum % 10}
 		cur = cur.Next
 	}
